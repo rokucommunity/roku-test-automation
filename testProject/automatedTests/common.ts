206615fe0c6727658be62b66c318ec42303024d0
Bronley Plumb <bronley@gmail.com>
2026-06-01 15:52:09 -0400

    Remove automated tests. Fix testProject brs deployment

--- testProject/automatedTests/common.ts
+++
@@ -1,9 +0,0 @@
-import { odc } from 'roku-test-automation';
-
-export async function waitForApplicationLoad() {
-	return odc.onFieldChangeOnce({
-		base: 'global',
-		keyPath: 'launchComplete',
-		match: true
-	});
-}
